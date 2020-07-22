8767bdadfe55e3bb7f29295fdf4a90c9e9fb0212
Kong Yang <kmongy05@gmail.com>
2020-07-22 16:54:32 -0500

    Updated animation on hamburger and overlay

--- bookmark-landing-page-master/src/main.js
+++ bookmark-landing-page-master/src/main.js
@@ -4,7 +4,9 @@ const hamburger = document.querySelector('.hamburger');
 
 function revealHamburgerNav() {
   const overlay = document.querySelector('.nav-overlay');
+
   overlay.classList.toggle('open');
+  hamburger.classList.toggle('open');
 }
 
 hamburger.addEventListener('click', revealHamburgerNav);
