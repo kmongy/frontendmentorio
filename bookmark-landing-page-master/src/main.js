4a6d9a5e2cdbd65ac3735aa3432d37a38464b81c
Kong Yang <kmongy05@gmail.com>
2020-07-22 17:41:08 -0500

    updated with hamburger menu reveal.

--- bookmark-landing-page-master/src/main.js
+++ bookmark-landing-page-master/src/main.js
@@ -1,12 +1,14 @@
 import '/scss/main.scss';
 
 const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav__links');
 
 function revealHamburgerNav() {
   const overlay = document.querySelector('.nav-overlay');
 
   overlay.classList.toggle('open');
   hamburger.classList.toggle('open');
+  navLinks.classList.toggle('open');
 }
 
 hamburger.addEventListener('click', revealHamburgerNav);
