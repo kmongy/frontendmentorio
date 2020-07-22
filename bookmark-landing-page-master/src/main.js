b5baaff975459bf4d573457893b178804509d7ce
Kong Yang <kmongy05@gmail.com>
2020-07-22 15:46:15 -0500

    updated hamburger click event. Started on the overlay.

--- bookmark-landing-page-master/src/main.js
+++ bookmark-landing-page-master/src/main.js
@@ -1,1 +1,10 @@
 import '/scss/main.scss';
+
+const hamburger = document.querySelector('.hamburger');
+
+function revealHamburgerNav() {
+  const overlay = document.querySelector('.nav-overlay');
+  overlay.classList.toggle('open');
+}
+
+hamburger.addEventListener('click', revealHamburgerNav);
