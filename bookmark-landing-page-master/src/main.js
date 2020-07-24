08be370f21c963a2a981aeed4dfbf0f0979ea448
Kong Yang <kmongy05@gmail.com>
2020-07-24 17:22:31 -0500

    updated with the tab js feature. Will need to do more styling fixes.

--- bookmark-landing-page-master/src/main.js
+++ bookmark-landing-page-master/src/main.js
@@ -3,6 +3,10 @@ import '/scss/main.scss';
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav__links');
 
+const tabButtons = document.querySelectorAll('[role="tab"]');
+const tabPanels = document.querySelectorAll('[role="tabpanel"]');
+const featuresTab = document.querySelector('.features__tabs');
+
 function revealHamburgerNav() {
   const overlay = document.querySelector('.nav-overlay');
 
@@ -11,4 +15,25 @@ function revealHamburgerNav() {
   navLinks.classList.toggle('open');
 }
 
+function handleClickTab(event) {
+  const buttonSpan = event.currentTarget.querySelector('span');
+  tabPanels.forEach((panel) => {
+    panel.hidden = true;
+  });
+  tabButtons.forEach((tab) => {
+    tab.setAttribute('aria-selected', false);
+  });
+
+  event.currentTarget.setAttribute('aria-selected', true);
+  buttonSpan.classList.add('active');
+
+  const id = event.currentTarget.id;
+  const tabPanel = featuresTab.querySelector(`[aria-labelledby="${id}"]`);
+
+  tabPanel.hidden = false;
+}
+
 hamburger.addEventListener('click', revealHamburgerNav);
+tabButtons.forEach((button) =>
+  button.addEventListener('click', handleClickTab)
+);
