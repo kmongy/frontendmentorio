85d34eb0cf84ab91a1089061ee4aeb371b2fb871
Kong Yang <kmongy05@gmail.com>
2020-10-02 16:42:55 -0500

    updated attribution. Started on js.

--- social-media-dash/main.js
+++ social-media-dash/main.js
@@ -1,1 +1,11 @@
 import './scss/main.scss';
+
+const toggleButton = document.querySelector('#toggle-button');
+
+function toggleDarkMode() {
+  console.log('DARK MODE');
+}
+
+toggleButton.addEventListener('click', toggleDarkMode);
+
+console.log("I'm ALIVE");
