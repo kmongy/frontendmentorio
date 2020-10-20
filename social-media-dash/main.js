7a3f61729101b1ab6fee0b642dd2a63cf6dda36b
Kong Yang <kmongy05@gmail.com>
2020-10-20 12:31:47 -0500

    updated with the start of the js.

--- social-media-dash/main.js
+++ social-media-dash/main.js
@@ -1,9 +1,11 @@
 import './scss/main.scss';
-
+const darkBG = document.querySelector('body');
+const socialMediaCard = document.querySelectorAll('.social-media-card');
 const toggleButton = document.querySelector('#toggle-button');
 
 function toggleDarkMode() {
-  console.log('DARK MODE');
+  darkBG.classList.toggle('darkModeBG');
+  socialMediaCard.classList.toggle('darkModeBG');
 }
 
 toggleButton.addEventListener('click', toggleDarkMode);
