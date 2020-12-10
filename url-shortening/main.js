641ee29c00dd375030821da90af2d5c1f57a7d35
Kong Yang <kmongy05@gmail.com>
2020-12-10 12:46:56 -0600

    Implementing the logic to fetch the shorten api data. Applied initial css for styling.

--- url-shortening/main.js
+++ url-shortening/main.js
@@ -3,8 +3,43 @@ import '/scss/main.scss';
 const mainNavigation = document.querySelector('.main-navigation');
 const hamburger = document.querySelector('.hamburger');
 
+const form = document.querySelector('#form-link-submission');
+const input = document.querySelector('#form-link');
+const linkResults = document.querySelector('.links-shortened-results');
+
 function expandMenu() {
   mainNavigation.classList.toggle('open');
 }
 
+function ipGenerator(event) {
+  event.preventDefault();
+  const formIP = input.value;
+  const shrtcodeAPI = `https://api.shrtco.de/v2/shorten?url=${formIP}`;
+
+  fetch(shrtcodeAPI)
+    .then((response) => response.json())
+    .then((data) => {
+      let div = document.createElement('div');
+      let originalIP = document.createElement('p');
+      let shortenedIP = document.createElement('p');
+      let copyButton = document.createElement('button');
+
+      div.classList.add('shortened-result');
+      originalIP.classList.add('originalIP');
+      shortenedIP.classList.add('shortenedIP');
+
+      originalIP.textContent = data.result.original_link;
+      shortenedIP.textContent = data.result.short_link;
+      copyButton.textContent = 'Copy';
+
+      linkResults.appendChild(div);
+      div.appendChild(originalIP);
+      div.appendChild(shortenedIP);
+      div.appendChild(copyButton);
+
+      console.log(data.result);
+    });
+}
+
 hamburger.addEventListener('click', expandMenu);
+form.addEventListener('submit', ipGenerator);
