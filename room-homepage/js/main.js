66f1405f13c342b4ef262c5749771dc5c167df5a
Kong Yang <kmongy05@gmail.com>
2021-01-06 14:21:08 -0600

    updated for mobile navigation. Still working on thatdarn overlay.

--- room-homepage/js/main.js
+++ room-homepage/js/main.js
@@ -1,3 +1,16 @@
 import '/scss/main.scss';
 
-console.log('HELLO there');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const hamburgerExit = document.querySelector('.hamburger-menu-exit');
+
+function toggleMobileMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  const menuOverlay = document.querySelector('.menu-overlay');
+  navLinks.classList.toggle('open');
+  navLinks.classList.toggle('block');
+
+  menuOverlay.classList.toggle('block');
+}
+
+hamburgerMenu.addEventListener('click', toggleMobileMenu);
+hamburgerExit.addEventListener('click', toggleMobileMenu);
