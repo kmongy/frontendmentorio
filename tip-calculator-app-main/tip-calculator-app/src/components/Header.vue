954f250822faab1d49ee8da1ab9ebf1bb73d7dc6
Kong Yang <kmongy05@gmail.com>
2021-10-02 11:13:30 -0500

    feat: total people input implemented.

--- tip-calculator-app-main/tip-calculator-app/src/components/Header.vue
+++ tip-calculator-app-main/tip-calculator-app/src/components/Header.vue
@@ -13,7 +13,7 @@ export default {
 <style lang="scss" scoped>
 .heading {
   color: hsl(186, 14%, 43%);
-  font-size: 1rem;
+  font-size: 0.9rem;
   text-align: center;
   text-transform: uppercase;
   letter-spacing: 0.75rem;
